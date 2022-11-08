3174bc9a0b9a9ac5fb23fea50c89658aa4de424f
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-08 10:20:50 -0800

    add red workflow to rollResult()
    Co-authored-by: Richard Cha <charichard09@gmail.com>

--- __tests__/pig-dice.test.js
+++ __tests__/pig-dice.test.js
@@ -1,8 +1,16 @@
-import { rollDi } from './../src/js/pig-dice.js';
+import { rollDi, rollResult } from './../src/js/pig-dice.js';
 
 describe('rollDi', () => {
 
   test('it should return a random number from 1-6', () => {
     expect(/[123456]/.test(rollDi())).toBeTruthy();
   });
+})
+
+describe('rollResult', () => {
+
+test('it should return 0 if a one is rolled', () => {
+  expect(rollResult(1)).toEqual(0);
+})
+
 })
